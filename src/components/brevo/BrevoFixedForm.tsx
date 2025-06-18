80ad45f0577c6cb7703bdd19b1e74cb440895b38
Selina Park <selina.park@aeternumproject.com>
2025-06-19 05:27:05 +0900

    improvement: short form section

--- src/components/brevo/BrevoFixedForm.tsx
+++ src/components/brevo/BrevoFixedForm.tsx
@@ -1,17 +1,20 @@
 export default function BrevoFixedForm() {
   return (
-    <iframe
-      width="100%"
-      height="700"
-      src="https://a276752d.sibforms.com/serve/MUIFAKJuKixrAE_EiW_XlkGWil_oERmtZFX4xgyzvMeIl4o6lujg4vIta2Uux6ONqLkpDU-0w-ENja5NsTN8i9rVSzfW5Z1Uexg136TiNsaze7Q9Me5YLzAPL0FFPeN7-Byao6THA3VaFNjC8EF2EL36TnY8GqyQ8Kymi-mYddVf5a2r9kwvbzqB7PMGNTeYx5Fha8u3lZYdt9na"
-      frameBorder="0"
-      scrolling="no"
-      allowFullScreen
-      style={{
-        display: "block",
-        margin: "0 auto",
-        maxWidth: "100%",
-      }}
-    ></iframe>
+    <div>
+      <iframe
+        width="100%"
+        height="550"
+        src="https://a276752d.sibforms.com/serve/MUIFAKJuKixrAE_EiW_XlkGWil_oERmtZFX4xgyzvMeIl4o6lujg4vIta2Uux6ONqLkpDU-0w-ENja5NsTN8i9rVSzfW5Z1Uexg136TiNsaze7Q9Me5YLzAPL0FFPeN7-Byao6THA3VaFNjC8EF2EL36TnY8GqyQ8Kymi-mYddVf5a2r9kwvbzqB7PMGNTeYx5Fha8u3lZYdt9na"
+        frameBorder="0"
+        scrolling="no"
+        allowFullScreen
+        style={{
+          display: "block",
+          margin: "0",
+          width: "100%", 
+          maxWidth: "100%", 
+        }}
+      ></iframe>
+    </div>
   );
 }
--- src/components/sections/SignUpSection.tsx
+++ src/components/sections/SignUpSection.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Image from "next/image";
-
 import BrevoFixedForm from "../brevo/BrevoFixedForm";
 
 declare global {
@@ -14,66 +13,59 @@ declare global {
 export default function SignUpSection() {
   return (
     <section
-      className="-mt-12 w-full relative h-fit"
+      className="lg:-mt-12 w-full relative h-fit lg:h-[550px]"
       aria-labelledby="signup-heading"
     >
-      <div className="md:m-8 flex flex-col md:flex-row overflow-hidden rounded-3xl bg-[#F7F8EA] md:min-h-[650px]">
-        <div className="md:flex-[2] flex flex-col justify-center items-center md:px-4 xl:px-36 bg-[url('/images/tree-fullwidth.jpg')] bg-cover bg-center rounded-3xl m-4 shadow-[4.0px_8.0px_8.0px_rgba(0,0,0,0.38)]">
-          <div className="flex justify-center items-center min-h-full">
-            <header
-              className="text-black text-center px-6 py-10  rounded-3xl"
-              style={{ backgroundColor: "rgba(247, 248, 234, 0.5)" }}
+      <div className="lg:mx-8 flex flex-col lg:flex-row overflow-hidden lg:rounded-3xl bg-[#F7F8EA] h-full lg:p-4">
+        <div
+          className="lg:w-1/2 relative flex items-center justify-center lg:rounded-3xl shadow-[4px_8px_8px_rgba(0,0,0,0.38)] overflow-hidden bg-cover bg-center min-h-[520px] lg:min-h-0 lg:h-full"
+          style={{ backgroundImage: "url('/images/tree.png')" }}
+        >
+          <div className="absolute inset-0 bg-[#F7F8EA]/80 w-full h-full lg:rounded-3xl" />
+
+          <div className="relative z-10 flex flex-col justify-center items-center text-black text-center px-4 py-8 lg:py-4 w-full h-full">
+            <h4
+              id="signup-heading"
+              className="mb-4 font-bold lg:font-normal tracking-tight"
             >
-              <h4
-                id="signup-heading"
-                className="mb-4 font-bold lg:font-normal tracking-tight"
-              >
-                Early Access to Aeternum
-              </h4>
-              <p className="mb-4">
-                Sign up as an Early Member{" "}
-                <span className="block lg:inline">
-                  and get exclusive early access to Aeternum,
-                </span>
-                <span className="block lg:inline">launching in 2025.</span>
-              </p>
-              <p className="mb-4 font-semibold">
-                As an Early Member, you will:
-              </p>
-              <ul className="text-left">
-                {[
-                  "Get an exclusive invitation to the Beta Test and shape the future of legacy preservation.",
-                  "Be the first to experience Aeternum through early access to the final version.",
-                  "Get exclusive updates on special offers, discount codes, events, and development progress.",
-                  "Get 50% off during the beta phase, plus 20% off an annual subscription when Aeternum officially launches.",
-                ].map((text, idx) => (
-                  <li
-                    key={idx}
-                    className="flex items-start gap-2 leading-normal tracking-normal"
-                  >
-                    <Image
-                      src="/icons/list.svg"
-                      alt="check icon"
-                      width={20}
-                      height={20}
-                      className="mt-[0.3rem]"
-                    />
-                    <span>{text}</span>
-                  </li>
-                ))}
-              </ul>
-            </header>
+              Early Access to Aeternum
+            </h4>
+            <p className="mb-4">
+              Sign up as an Early Adopter and get exclusive early access to
+              Aeternum.
+            </p>
+            <p className="mb-4 font-semibold">As an Early Adopter, you will:</p>
+            <ul className="text-left max-w-lg">
+              {[
+                "Get an exclusive invitation to the Beta Test and shape the future of legacy preservation.",
+                "Be the first to experience Aeternum through early access to the final version.",
+                "Get exclusive updates on special offers, discount codes, events, and development progress.",
+                "Get 50% off during the beta phase, plus 20% off an annual subscription when Aeternum officially launches.",
+              ].map((text, idx) => (
+                <li
+                  key={idx}
+                  className="flex items-start gap-2 leading-normal tracking-normal mb-2"
+                >
+                  <Image
+                    src="/icons/list.svg"
+                    alt="check icon"
+                    width={20}
+                    height={20}
+                    className="mt-[0.3rem]"
+                  />
+                  <span>{text}</span>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
+
         <div
           id="signup"
-          className="flex-1 flex items-center justify-center rounded-3xl overflow-hidden 
-             min-h-[550px] md:min-h-[650px]"
+          className="lg:w-1/2 flex items-center justify-center overflow-hidden h-full"
         >
-          <div className="flex justify-center items-center max-w-[540px] h-[480px] md:h-full">
-            <div className="mt-32">
-              <BrevoFixedForm />
-            </div>
+          <div className="flex justify-center items-center w-full h-full">
+            <BrevoFixedForm />
           </div>
         </div>
       </div>
