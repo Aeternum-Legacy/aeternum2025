5501cfff40c55c02bf6fb71d061f18832ead4aae
Selina Park <selina.park@aeternumproject.com>
2025-06-19 23:31:40 +0900

    hotfix

--- src/app/robots.txt/route.ts
+++ src/app/robots.txt/route.ts
@@ -3,7 +3,12 @@ import { NextRequest } from "next/server";
 export async function GET(req: NextRequest) {
   const baseURL = process.env.WORDPRESS_API_URL!;
   const res = await fetch(`${baseURL}/robots.txt`);
-  const txt = await res.text();
+  let txt = await res.text();
+
+  txt = txt.replace(
+    /Sitemap: http:\/\/api\.aeternumproject\.com\/sitemap_index\.xml/,
+    "Sitemap: https://aeternumproject.com/sitemap.xml"
+  );
 
   return new Response(txt, {
     status: 200,
--- src/app/sitemap.xml/route.ts
+++ src/app/sitemap.xml/route.ts
@@ -3,6 +3,7 @@ import { NextRequest } from "next/server";
 export async function GET(req: NextRequest) {
   const baseURL = process.env.WORDPRESS_API_URL!;
   const res = await fetch(`${baseURL}/sitemap_index.xml`);
+
   const xml = await res.text();
 
   return new Response(xml, {
