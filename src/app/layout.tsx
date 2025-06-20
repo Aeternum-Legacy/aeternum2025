b352a6392973a602490df5172784d25d9f5ead46
Selina Park <selina.park@aeternumproject.com>
2025-06-20 04:30:41 -0500

    GTM added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,20 +39,20 @@ export default function RootLayout({
           href="https://sibforms.com/forms/end-form/build/sib-styles.css"
         />
 
-        {/* Meta (Facebook) Pixel */}
+        {/* Facebook Pixel */}
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}
-    (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}');
-    fbq('track', 'PageView');
-  `}
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
         </Script>
         <noscript>
           <img
@@ -82,7 +82,7 @@ export default function RootLayout({
           `}
         </Script>
 
-        {/* Brevo SDK */}
+        {/* Brevo Popup SDK */}
         <Script
           src="https://cdn.brevo.com/js/sdk-loader.js"
           strategy="afterInteractive"
@@ -105,9 +105,30 @@ export default function RootLayout({
             Brevo.push(["showForm"]);
           `}
         </Script>
+
+        {/* Google Tag Manager */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+          `}
+        </Script>
       </head>
 
       <body className="bg-pattern">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <MobileNavProvider>
           <NavbarDemo />
           <Suspense fallback={null}>
