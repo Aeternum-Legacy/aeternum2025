cf877fbb278f42e9f79f1ce3513354ec7ee495b4
Selina Park <selina.park@aeternumproject.com>
2025-07-22 07:55:28 -0500

    fix robots.txt

--- src/app/robots.txt/route.ts
+++ src/app/robots.txt/route.ts
@@ -1,14 +1,13 @@
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const baseURL = process.env.WORDPRESS_API_URL!;
-  const res = await fetch(`${baseURL}/robots.txt`);
-  let txt = await res.text();
+  const txt = `
+User-agent: *
+Allow: /
 
-  txt = txt.replace(
-    /Sitemap: http:\/\/api\.aeternumproject\.com\/sitemap_index\.xml/,
-    "Sitemap: https://aeternumproject.com/sitemap.xml"
-  );
+Sitemap: https://aeternumproject.com/sitemap.xml
+Sitemap: https://aeternumproject.com/blog/sitemap.xml
+  `.trim();
 
   return new Response(txt, {
     status: 200,
