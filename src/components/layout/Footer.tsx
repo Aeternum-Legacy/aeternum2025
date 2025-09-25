e2f738f35458ba4571d4e6484f4abf9423bd6abd
Selina Park <selina.park@aeternumproject.com>
2025-09-25 14:28:00 -0400

    delete brevoform file

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,6 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect } from "react";
-import BrevoFixedForm from "../brevo/BrevoFixedForm";
 
 export default function Footer() {
   useEffect(() => {
