96c52f29346d86dd78ee8466ee534e356517508f
Selina Park <selina.park@aeternumproject.com>
2025-09-25 14:24:54 -0400

    fix: brevo form cut off issue

--- src/components/sections/SignUpSection.tsx
+++ src/components/sections/SignUpSection.tsx
@@ -1,7 +1,5 @@
 "use client";
-
 import Image from "next/image";
-import BrevoFixedForm from "../brevo/BrevoFixedForm";
 
 declare global {
   interface Window {
@@ -13,17 +11,25 @@ declare global {
 export default function SignUpSection() {
   return (
     <section
-      className="lg:-mt-12 w-full relative h-fit lg:h-[550px]"
       aria-labelledby="signup-heading"
+      className="w-full h-fit lg:h-[550px]"
     >
-      <div className="lg:mx-8 flex flex-col lg:flex-row overflow-hidden lg:rounded-3xl bg-[#F7F8EA] h-full lg:p-4">
+      <div
+        className="
+      lg:mx-8 flex flex-col lg:flex-row overflow-hidden lg:rounded-3xl
+      bg-[#F7F8EA] h-fit lg:h-[550px]   
+    "
+      >
         <div
-          className="lg:w-1/2 relative flex items-center justify-center lg:rounded-3xl shadow-[4px_8px_8px_rgba(0,0,0,0.38)] overflow-hidden bg-cover bg-center min-h-[520px] lg:min-h-0 lg:h-full"
+          className="
+        lg:w-1/2 relative flex items-center justify-center 
+        shadow-[4px_8px_8px_rgba(0,0,0,0.38)] overflow-hidden bg-cover bg-center
+        min-h-[320px] lg:h-[550px]      // 모바일에서는 최소 높이만 유지
+      "
           style={{ backgroundImage: "url('/images/tree.png')" }}
         >
-          <div className="absolute inset-0 bg-[#F7F8EA]/80 w-full h-full lg:rounded-3xl" />
-
-          <div className="relative z-10 flex flex-col justify-center items-center text-black text-center px-4 py-8 lg:py-4 w-full h-full">
+          <div className="absolute inset-0 bg-[#F7F8EA]/80 w-full h-full " />
+          <div className="relative z-10 flex flex-col justify-center items-center text-black text-center px-4 py-8 w-full h-full">
             <h4
               id="signup-heading"
               className="mb-4 font-bold lg:font-normal tracking-tight"
@@ -62,10 +68,20 @@ export default function SignUpSection() {
 
         <div
           id="signup"
-          className="lg:w-1/2 flex items-center justify-center overflow-hidden h-full"
+          className="
+        lg:w-1/2 grid place-items-center
+        h-[500px] lg:h-[550px]        
+      "
         >
-          <div className="flex justify-center items-center w-full h-full">
-            <BrevoFixedForm />
+          <div className="w-full h-full min-h-[320px] flex items-center justify-center overflow-hidden">
+            <iframe
+              src="https://a276752d.sibforms.com/serve/MUIFADvHCOpCEY3oWkUMEl0bNTu2r09khoCDyqLgkEe-hOlEgdxdU9gPyTRaP0e1NOFFQeYwTKPB7rZm6sJnRQGrPoylhMtagAyY-FnpUKGfAev6xW8VHhNDQNGMruySbHNJgaLOeJ4BhhmHLE5tvatv5Iz6qYRHOKX9U65whVcBOdcEvOSjPU1R_-caY6olFWkwnLc4WzzpQMTL"
+              className="block w-full h-full"
+              style={{ border: 0, maxWidth: "100%", maxHeight: "100%" }}
+              scrolling="no"
+              frameBorder={0}
+              allowFullScreen
+            />
           </div>
         </div>
       </div>
